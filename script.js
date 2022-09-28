4355cc1b0c127bb7e79b91ba04668effff3379a5
hiren patel <hirenpatel0526@yahoo.com>
2022-09-28 14:28:00 -0700

    fetch

---
+++ script.js
@@ -0,0 +1,6 @@
+// e46479163adf8296196ce28dcb4704e5
+
+
+fetch('https://api.themoviedb.org/3/movie/550?api_key=e46479163adf8296196ce28dcb4704e5')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
