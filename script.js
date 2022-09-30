e2ee65ab28d32d3422561d37b301a97174cc2f50
hiren patel <hirenpatel0526@yahoo.com>
2022-09-30 07:52:22 -0700

    eventlistener to search btn

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function collectInfo(){
   var genreSel = document.getElementById("selection");
  }
 
+ document.getElementById("searchBtn").addEventListener("click", collectInfo())
+console.log(collectInfo)
 
 // using fetch method to retrieve data from Api and giving condition to work on
 fetch('https://api.themoviedb.org/3/movie/550?api_key=cb0a27fc2effad207407997a55dc7c30')
@@ -15,26 +17,18 @@ fetch('https://api.themoviedb.org/3/movie/550?api_key=cb0a27fc2effad207407997a55
 fetch('http://www.omdbapi.com/?i=tt3896198&apikey=4fca7de8')
 .then((response) => response.json())
 .then((data) => console.log(data));
-
-
-
-
-collectInfo();
-
-
-
-
-
 // {
-//   for(i=0; i<5; i++){
-//       document.getElementById("day" + (i+1) + "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min-293.2).toFixed(2)+ "°";
-//   }
-//   for(i=0; i<5; i++){
-//       document.getElementById("day" + (i+1) + "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max-293.2).toFixed(2)+ "°";
-//   }
-//   // this link is not working below-------------
-//   for (var i = 0; i < 5; i++){
-//       document.getElementById("img" + (i+1)).src ="http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon +".png";
-//   }
-// })
+  // for(i = 0; i < fetch.length; i++){
+  //       document.getElementById("movieTitle").innerHTML = "Title" +(fetch.list);
+  // }
+    // for( i= 0; i < 5; i++){
+    //       document.getElementById("day" + (i+1) + "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max-293.2).toFixed(2)+ "°";
+    //   }
+    //   // this link is not working below-------------
+    //   for (var i = 0; i < 5; i++){
+    //         document.getElementById("img" + (i+1)).src ="http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon +".png";
+    //     }
+      // }
+    
       
+      collectInfo();
