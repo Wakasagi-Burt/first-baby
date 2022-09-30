4a084bafc4bb7fb99dcccf242449fc2518f0ce84
ALAN JOSE <alanjose04231120@gmail.com>
2022-09-30 14:05:00 -0400

    problems with the search function

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 //        // declared function collectioninfo to fetch information from API key
-function collectInfo(){
-  var newList = document.getElementById("movie-search");
-  var movieTitle = document.getElementById("movie-input");
-  var genreSel = document.getElementById("selection");
- }
+var newList = document.getElementById("movie-search");
 
- document.getElementById("searchBtn").addEventListener("click", collectInfo())
-console.log(collectInfo)
+var genreSel = document.getElementById("selection");
+document.getElementById("searchBtn").addEventListener("click", collectInfo);
+
+function collectInfo(event) {
+  event.preventDefault();
+  movieTitle = document.getElementById("movie-input").value;
+  var stringval = ;
+  console.log(movieTitle.toString());
+  fetch('https://api.themoviedb.org/3/search/movie?api_key=cb0a27fc2effad207407997a55dc7c30&language=en-US&query='+ stringval +"&page=1&include_adult=false")
+  .then((response) => response.json())
+  .then(function (data) {
+    console.log(data)
+    // var example = document.getElementById("example-title");
+    // example.textContent = data.title;
+    // example.textContent = data.title;
+    // var image = document.getElementById("example-image");
+    // image.src ='https://image.tmdb.org/t/p/w300_and_h450_bestv2/'+data.poster_path;
+ })
+}
+
+
+
+
+
 
        // using fetch method to retrieve data from Api and giving condition to work on
-fetch('https://api.themoviedb.org/3/movie/550?api_key=cb0a27fc2effad207407997a55dc7c30')
-.then((response) => response.json())
-.then(function (data){
-  console.log(data);
-  var example = document.getElementById("example-title");
-  example.textContent = data.title;
-  example.textContent = data.title;
-  var image = document.getElementById("example-image");
-  image.src ='https://image.tmdb.org/t/p/w300_and_h450_bestv2/'+data.poster_path;
-})
+// fetch('https://api.themoviedb.org/3/search/movie?api_key=cb0a27fc2effad207407997a55dc7c30&language=en-US&query='+ movieTitle +"&page=1&include_adult=false")
+// .then((response) => response.json())
+// .then(function (data){
+//   console.log(data);
+  // var example = document.getElementById("example-title");
+  // example.textContent = data.title;
+  // example.textContent = data.title;
+  // var image = document.getElementById("example-image");
+  // image.src ='https://image.tmdb.org/t/p/w300_and_h450_bestv2/'+data.poster_path;
+// })
 
-       // using another fetchmethod to get info
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=4fca7de8')
-.then((response) => response.json())
-.then((data) => console.log(data));
-
-collectInfo();
-  
+//        // using another fetchmethod to get info
+// fetch('http://www.omdbapi.com/?i=tt3896198&apikey=4fca7de8')
+// .then((response) => response.json())
+// .then((data) => console.log(data));
