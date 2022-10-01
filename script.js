f69473fbaf276a6c6a6370e894a9d03f85bf6acd
ALAN JOSE <alanjose04231120@gmail.com>
2022-10-01 16:37:14 -0400

    created search feature with carousel display

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //        // declared function collectioninfo to fetch information from API key
 var newList = document.getElementById("movie-search");
-
+var example = document.getElementById("example-title");
 var genreSel = document.getElementById("selection");
 document.getElementById("searchBtn").addEventListener("click", collectInfo);
+document.getElementById("nextbtn").addEventListener("click", nextpage);
+document.getElementById("nextbtn").addEventListener("click", followingpage);
 
 function collectInfo(event) {
   event.preventDefault();
@@ -13,17 +15,19 @@ function collectInfo(event) {
   .then((response) => response.json())
   .then(function (data) {
    console.log(data.results[0])
-    // var example = document.getElementById("example-title");
-    // example.textContent = data.title;
-    // example.textContent = data.title;
-    // var image = document.getElementById("example-image");
-    // image.src ='https://image.tmdb.org/t/p/w300_and_h450_bestv2/'+data.poster_path;
+   console.log(data.results[0].title);
+   
+   example.textContent = data.results[0].title;
+   var movieid = data.results[0].poster_path;
+   console.log(movieid);
+   var image = document.getElementById("example-image");
+  image.src ="https://image.tmdb.org/t/p/w185/"+ data.results[0].poster_path;
+  var image2 = document.getElementById("im2");
+  var image3 = document.getElementById("im3");
+  image2.src = "https://image.tmdb.org/t/p/w185" + data.results[1].poster_path;
+  image3.src = "https://image.tmdb.org/t/p/w185" + data.results[2].poster_path;
 })
  }
-
-
-
-
 
 
        // using fetch method to retrieve data from Api and giving condition to work on
@@ -41,4 +45,4 @@ function collectInfo(event) {
 //        // using another fetchmethod to get info
 // fetch('http://www.omdbapi.com/?i=tt3896198&apikey=4fca7de8')
 // .then((response) => response.json())
-// .then((data) => console.log(data));
+// .then((data) => console.log(data)):
